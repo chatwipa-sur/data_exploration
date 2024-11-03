ffa74baa056fed16b02c1979a5a18ccaaa27573c
chatwipa.s <chatwipa.sur@gmail.com>
2024-11-03 21:17:22 +0000

    [Add] combine enrolments file at the pre-processing step

--- coursework_CSC8631/munge/01-Combined_leave.R
+++ coursework_CSC8631/munge/01-Combined_leave.R
@@ -2,11 +2,11 @@ library(dplyr)
 library(stringr)
 library(lubridate)
 
-# Select and store data frames
-dfs <- mget(ls(pattern = "leaving.survey.response"))
+# ---------------- Select leaving survey response ---------------- #
+leave_dfs <- mget(ls(pattern = "leaving.survey.response"))
 
 # Convert columns to the same data type and store all data frame in one list
-dfs <- Map(function(df, name) {
+leave_dfs <- Map(function(df, name) {
   df$run_number <- as.integer(str_extract(name, "\\d+"))
   df$learner_id <- as.character(df$learner_id)
   df$leaving_reason <- as.character(df$leaving_reason)
@@ -15,10 +15,31 @@ dfs <- Map(function(df, name) {
   df$left_at <- ymd_hms(df$left_at, quiet = TRUE)
   df$last_completed_step_at <- ymd_hms(df$last_completed_step_at, quiet = TRUE)
   return(df)
-}, dfs, names(dfs))
+}, leave_dfs, names(leave_dfs))
 
 # Combine all data frames into one
-all_leaving_survey <- bind_rows(dfs)
+leave_survey_all <- bind_rows(leave_dfs)
+
+# ---------------- Select enrollment ---------------- #
+enroll_dfs <- mget(ls(pattern = "enrolments"))
+
+# Convert columns to the same data type and store all data frame in one list
+enroll_dfs <- Map(function(df, name) {
+  df$run_number <- as.integer(str_extract(name, "\\d+"))
+  df$learner_id <- as.character(df$learner_id)
+
+  # Time stamp handling
+  df$enrolled_at <- ymd_hms(df$enrolled_at, quiet = TRUE)
+  df$unenrolled_at <- ymd_hms(df$unenrolled_at, quiet = TRUE)
+  df$fully_participated_at <- ymd_hms(df$fully_participated_at, quiet = TRUE)
+  df$purchased_statement_at <- ymd_hms(df$purchased_statement_at, quiet = TRUE)
+  return(df)
+}, enroll_dfs, names(enroll_dfs))
+
+# Combine all data frames into one
+enroll_survey_all <- bind_rows(enroll_dfs)
+
+preprocessed_leave <- full_join(enroll_survey_all, leave_survey_all, by = c("learner_id", "run_number"))
 
 # Cache the combined data frame
-cache('all_leaving_survey')
+cache('preprocessed_leave')
