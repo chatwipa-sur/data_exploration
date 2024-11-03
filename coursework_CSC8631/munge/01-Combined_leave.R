f809a8882414a7cf4de5ead34a10dba4ba8e3efd
chatwipa.s <chatwipa.sur@gmail.com>
2024-11-03 09:23:51 +0000

    [Fix bug] duplicate of data when re-preprocess

--- coursework_CSC8631/munge/01-Combined_leave.R
+++ coursework_CSC8631/munge/01-Combined_leave.R
@@ -1,20 +1,24 @@
-# Un-enroll combine table
-library('dplyr')
+library(dplyr)
+library(stringr)
+library(lubridate)
 
 # Select and store data frames
-dfs <- mget(ls(pattern = "leaving.survey"))
+dfs <- mget(ls(pattern = "leaving.survey.response"))
 
-# Convert all column to be same type of data
-dfs <- lapply(dfs, function(df) {
+# Convert columns to the same data type and store all data frame in one list
+dfs <- Map(function(df, name) {
+  df$run_number <- as.integer(str_extract(name, "\\d+"))
   df$learner_id <- as.character(df$learner_id)
   df$leaving_reason <- as.character(df$leaving_reason)
-  df$left_at <- as.POSIXct(df$left_at, origin = "1970-01-01", tz = "UTC")
-  df$last_completed_step_at <- as.POSIXct(df$left_at, origin = "1970-01-01", tz = "UTC")
-  df$year <- as.integer(format(df$left_at, "%Y"))
+  
+  # Time stamp handling
+  df$left_at <- ymd_hms(df$left_at, quiet = TRUE)
+  df$last_completed_step_at <- ymd_hms(df$last_completed_step_at, quiet = TRUE)
   return(df)
-})
+}, dfs, names(dfs))
 
-
+# Combine all data frames into one
 all_leaving_survey <- bind_rows(dfs)
 
+# Cache the combined data frame
 cache('all_leaving_survey')
